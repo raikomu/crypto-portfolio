53d49163858663f07a731f192a6326b958aa8763
Raiko Murulauk <raiko4238@gmail.com>
2020-02-15 18:28:49 +0200

    Test Bitfinex request

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -5,17 +5,4 @@ class Currency < ApplicationRecord
   validates :amount, numericality: { only_integer: true, :greater_than => 0 }
   validates :wallet, presence: true, length: { maximum: 320 }
 
-  def market_value(currency = name)
-    client = Bitfinex::RESTv2.new({
-      :api_key => 'APuOTD6nNWv12JuVe3gBZW0yEnCdhmE7PqBSxFUvpKr',
-      :api_secret => 'cuq2NJIrskU1IkLr1VKfrw0qQm71pOsWZYFFDnafg89',
-    })
-    sym = 
-    case name
-      when "Bitcoin" then "BTC"
-      when "Ethereum" then "ETH"
-      when "Ripple" then "XRP"
-    end
-    client.ticker("t#{sym}USD")[-4] * amount.round(4)
-  end
 end

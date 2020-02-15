0cd554ea7878c6c3eb0375c613a4fe185555b5e1
Raiko Murulauk <raiko4238@gmail.com>
2020-02-15 19:07:35 +0200

    Test Bitfinex API

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,7 +4,6 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-import axios from 'axios'
 import VueAxios from 'vue-axios'
 import { securedAxiosInstance, plainAxiosInstance } from './backend/axios'
 
@@ -15,7 +14,7 @@ Vue.config.productionTip = false
 Vue.use(VueAxios, {
   secured: securedAxiosInstance,
   plain: plainAxiosInstance
-}, axios)
+})
 
 /* eslint-disable no-new */
 new Vue({

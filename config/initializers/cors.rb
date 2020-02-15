1563aff31221c728f225cfc8f1a26ea4b6bde377
Raiko Murulauk <raiko4238@gmail.com>
2020-02-15 13:21:00 +0200

    Fix CORS for Heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080'
+    origins ['http://localhost:8080', 'https://mysterious-fortress-90505.herokuapp.com']
 
     resource '*',
       headers: :any,
